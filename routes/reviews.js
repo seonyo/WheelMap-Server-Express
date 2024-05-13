6144bc2671478f15f31fc5ac4d3ff1a129a4e28f
seonyo <jsy6535@naver.com>
2024-05-14 00:49:54 +0900

    Feat: 충전소 이름으로 리뷰 찾는 API 구현

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Review } = require('../models');
-const { Op } = require("sequelize");
+const { Op, where } = require("sequelize");
 
 const router = express.Router();
 
@@ -17,6 +17,26 @@ router.post('/', async (req, res) => {
     } catch (error) {
         console.log(error.message)
         res.status(500).json({ error: "서버에서 에러 발생" });
+    }
+});
+
+//충전소 이름으로 리뷰 찾기
+router.get('/:charger_name',  async(req, res) => {
+    try{
+        const charger_name = req.params.charger_name;
+
+        const charger_infos = await Review.findAll({
+            where: {charger_name}
+        });
+
+        if(charger_infos.length == 0){
+            return res.status(404).json({message : "해당 충전소의 리뷰를 찾을 수 없습니다."})
+        }else{
+            return res.status(200).json(charger_infos);
+        }
+    }catch (error){
+        console.log(error.message);
+        return res.status(500).json({error : "서버에서 에러 발생"})
     }
 });
 
