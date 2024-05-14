7474d2b18d36bd8440f3d0fa51ff6b43af26b21b
seonyo <jsy6535@naver.com>
2024-05-14 10:36:25 +0900

    Feat: user_id로 리뷰 정보 불러오는 API 구현

--- routes/reviews.js
+++ routes/reviews.js
@@ -21,22 +21,41 @@ router.post('/', async (req, res) => {
 });
 
 //충전소 이름으로 리뷰 찾기
-router.get('/charger/:charger_name',  async(req, res) => {
-    try{
+router.get('/charger/:charger_name', async (req, res) => {
+    try {
         const charger_name = req.params.charger_name;
 
         const charger_infos = await Review.findAll({
-            where: {charger_name}
+            where: { charger_name }
         });
 
-        if(charger_infos.length == 0){
-            return res.status(404).json({message : "해당 충전소의 리뷰를 찾을 수 없습니다."})
-        }else{
+        if (charger_infos.length == 0) {
+            return res.status(404).json({ message: "해당 충전소의 리뷰를 찾을 수 없습니다." })
+        } else {
             return res.status(200).json(charger_infos);
         }
-    }catch (error){
+    } catch (error) {
         console.log(error.message);
-        return res.status(500).json({error : "서버에서 에러 발생"})
+        return res.status(500).json({ error: "서버에서 에러 발생" })
+    }
+});
+
+//user_id로 리뷰 찾기
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const user_id = req.params.user_id;
+
+        const charger_infos = await Review.findAll({
+            where: { user_id }
+        });
+        if (charger_infos.length == 0) {
+            return res.status(404).json({ message: "해당 충전소의 리뷰를 찾을 수 없습니다." })
+        } else {
+            return res.status(200).json(charger_infos);
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ error: "서버에서 에러 발생" })
     }
 });
 
